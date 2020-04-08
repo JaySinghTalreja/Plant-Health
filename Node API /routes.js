45a681c8bf499a6be228ed198cb0be66bcb48d79
JaySinghX <jaysinghtalreja@gmail.com>
2020-04-08 16:56:58 +0530

    Upload DOne

--- Node API /routes.js
+++ Node API /routes.js
@@ -4,17 +4,27 @@ var multer  = require('multer')
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/Uploads')
+      cb(null, 'Uploads/')
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
       cb(null, file.fieldname + '-' + uniqueSuffix)
     }
+  });
+
+var upload = multer({ storage: storage }).single('testImage')
+
+router.post('/predict', function (req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+    } else if (err) {
+      // An unknown error occurred when uploading.
+    }
+
+    // Everything went fine.
   })
-  
-  var upload = multer({ storage: storage })
-
-
+});
 
 router.get('', function(request, response) {
     response.json({
@@ -27,10 +37,5 @@ router.get('predict', function(request, response){
         "error" : "0",
         "message" : "PLANT DISEASE DETECTION API By Jay Singh Talreja"
     });
-});
-router.post('predict', async function(request, response) {
-    if(request.body) {
-        var requestData = 
-    }
 });
 module.exports = router;
