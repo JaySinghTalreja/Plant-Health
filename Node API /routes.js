1e4a825a306a112fbabe8168aeedae9bf68ba534
JaySinghX <jaysinghtalreja@gmail.com>
2020-04-08 14:39:52 +0530

    Added changes

--- Node API /routes.js
+++ Node API /routes.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
+var multer  = require('multer')
+
 router.get('', function(request, response) {
     response.json({
         STATUS:"WORKING",
         MSG:"API STARTED"
     });
 });
+router.get('predict', function(request, response){
+    response.json({
+        "error" : "0",
+        "message" : "PLANT DISEASE DETECTION API By Jay Singh Talreja"
+    });
+});
 router.post('predict', async function(request, response) {
-    
+    if(request.body) {
+        var requestData = 
+    }
 });
 module.exports = router;
