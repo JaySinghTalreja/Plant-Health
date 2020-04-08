279cc3a37e22b2b9b85ef6e413ffa82712c0e273
JaySinghX <jaysinghtalreja@gmail.com>
2020-04-08 16:43:32 +0530

    Uploads Directory

--- Node API /routes.js
+++ Node API /routes.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 var multer  = require('multer')
 
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/Uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  var upload = multer({ storage: storage })
+
+
+
 router.get('', function(request, response) {
     response.json({
         STATUS:"WORKING",
