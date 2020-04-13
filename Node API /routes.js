6e257ac56d1889cef7e8b1584ddbaed5f1b102c5
JaySinghX <jaysinghtalreja@gmail.com>
2020-04-13 16:43:54 +0530

    More Modifications

--- Node API /routes.js
+++ Node API /routes.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 var multer  = require('multer')
 
-
+var outFileName;
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'Uploads/')
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      outFileName = file.fieldname+'-'+uniqueSuffix;
       cb(null, file.fieldname + '-' + uniqueSuffix)
     }
   });
@@ -18,8 +19,23 @@ router.post('/predict', function (req, res) {
   upload(req, res, function (err) {
     if (err instanceof multer.MulterError) {
       // A Multer error occurred when uploading.
+      res.json({
+        STATUS:"ERROR",
+        MSG:"Multer Error"
+    });
     } else if (err) {
       // An unknown error occurred when uploading.
+      res.json({
+        STATUS:"Error",
+        MSG:"Unknown Error"
+        });
+    }
+    else{
+        
+        res.json({
+            STATUS:"File Uploaded",
+            FILENAME:outFileName
+        });
     }
 
     // Everything went fine.
@@ -32,7 +48,7 @@ router.get('', function(request, response) {
         MSG:"API STARTED"
     });
 });
-router.get('predict', function(request, response){
+router.get('/predict', function(request, response){
     response.json({
         "error" : "0",
         "message" : "PLANT DISEASE DETECTION API By Jay Singh Talreja"
