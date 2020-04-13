fd875770cd01f494dad423b6e815ee9d3feb1503
JaySinghX <jaysinghtalreja@gmail.com>
2020-04-13 17:43:26 +0530

    mods

--- lib/ui/gallery_select.dart
+++ lib/ui/gallery_select.dart
@@ -1,15 +1,34 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 class GallerySelect extends StatefulWidget {
   @override
   _GallerySelectState createState() => _GallerySelectState();
 }
 
 class _GallerySelectState extends State<GallerySelect> {
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = image;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Center(
-        child: new Text("Gallery Screen"),
+      body: Center(
+        child: _image == null ? Text('No image selected.'): Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
       ),
     );
   }
--- lib/ui/live_cam.dart
+++ lib/ui/live_cam.dart
@@ -1,16 +1,58 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:progress_button/progress_button.dart';
 class LiveCam extends StatefulWidget {
   @override
   _LiveCamState createState() => _LiveCamState();
 }
 
 class _LiveCamState extends State<LiveCam> {
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Center(
-        child: new Text("Live Cam Screen"),
-      ),
-    );
+    return new MaterialApp(
+      title:"Live Cam Screen"
+      theme : new ThemeData(
+        primaryColor: Colors.lightGreen,
+        primarySwatch: Colors.blue,
+        accentColor: Colors.red
+        ),
+          //Tu Remove the Debig Tag
+        debugShowCheckedModeBanner: false,
+        home : new Scaffold(
+          body: Center(
+            child: _image == null ? Text('No image selected.'): Image.file(_image),
+          ),
+          
+          theme: new ThemeData(
+              primaryColor: Colors.lightGreen,
+              primarySwatch: Colors.blue,
+              accentColor: Colors.red
+          ),
+
+          floatingActionButton : ProgressButton(
+            child: Text("Get Image"),
+            onPressed: getImage,
+            buttonState: ButtonState.normal,
+            backgroundColor: Theme.of(context).primaryColor,
+            progressColor: Theme.of(context).primaryColor,
+          ),
+
+          /*floatingActionButton: FloatingActionButton(
+            onPressed: getImage,
+            tooltip: 'Pick Image',
+            child: Icon(Icons.add_a_photo),
+          ),*/
+        );
+      );
   }
 }
