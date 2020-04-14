b54ae927326ae377510f520133f0cb10b30f145f
JaySinghX <jaysinghtalreja@gmail.com>
2020-04-14 12:14:59 +0530

    Shake Animation On FLoatingActionButton

--- lib/ui/live_cam.dart
+++ lib/ui/live_cam.dart
@@ -2,13 +2,18 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:progress_button/progress_button.dart';
+import 'package:animated_widgets/animated_widgets.dart';
 class LiveCam extends StatefulWidget {
   @override
   _LiveCamState createState() => _LiveCamState();
 }
 
 class _LiveCamState extends State<LiveCam> {
+  //Image
   File _image;
+
+  //Bool for Shake Animated Widget
+  bool _enabled = true;
 
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
@@ -39,15 +44,33 @@ class _LiveCamState extends State<LiveCam> {
         floatingActionButton: Padding(
           padding: const EdgeInsets.only(bottom: 20.0),
           child: FloatingActionButton(
-          onPressed: getImage,
-          tooltip: 'Pick Image',
-          child: new IconTheme(
-            data: new IconThemeData(color: Colors.white), 
-            child: new Icon(Icons.add_a_photo),
-          ),
+            onPressed: getImage,
+            tooltip: 'Pick Image',
+            child: new IconTheme(
+              data: new IconThemeData(color: Colors.white), 
+              child: new Icon(Icons.add_a_photo),
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+ShakeAnimatedWidget(
+  enabled: this._enabled,
+  duration: Duration(milliseconds: 1500),
+  shakeAngle: Rotation.deg(z: 40),
+  curve: Curves.linear,
+  child: FloatingActionButton(
+    onPressed: getImage,
+    tooltip: 'Pick Image',
+    child: new IconTheme(
+      data: new IconThemeData(color: Colors.white), 
+      child: new Icon(Icons.add_a_photo),
+    ),
+  ),
+),
+/*
+*/
+
