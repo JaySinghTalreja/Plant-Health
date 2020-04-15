0d85b6f3d26e5194bea1e3462b4a3dcc365c4735
JaySinghX <jaysinghtalreja@gmail.com>
2020-04-15 16:33:49 +0530

    Fixed State Change Issue with the Argon Button

--- lib/ui/gallery_select.dart
+++ lib/ui/gallery_select.dart
@@ -14,8 +14,9 @@ class GallerySelect extends StatefulWidget {
 
 class _GallerySelectState extends State<GallerySelect> {
   //Image Variable
-  File _image;
+  static File _image;
 
+  static File tempImage;
   
 
   Future getImage() async {
@@ -25,48 +26,51 @@ class _GallerySelectState extends State<GallerySelect> {
     });
   }
 
-  //Argon Button
-  var argonButton = ArgonButton(
-    height: 50,
-    width: 200,
-    borderRadius: 5.0,
-    color: Colors.lightGreen,
-    child: Text(
-      "Select From Gallery",
-      style: TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-          fontWeight: FontWeight.w700
-    ),
-    ),
-    loader: Container(
-      padding: EdgeInsets.all(10),
-      child: SpinKitRotatingCircle(
-        color: Colors.white,
-        // size: loaderWidth ,
-      ),
-    ),
-    onTap: (startLoading, stopLoading, btnState) async {
-      if(btnState == ButtonState.Idle){
-        startLoading();
-        //Default Func
-        //await doNetworkRequest();
-        //Delaying for the loading circle animation
-        await new Future.delayed(const Duration(seconds : 2));
-        
-        var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-        stopLoading();
-      }
-    },
-  );
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Center(
+        //--------------------------------------------------------Issues Commented-------------------------
         //child: _image == null ? Text('No image selected.'): Image.file(_image),
         //Removed the Image Switch with button so that we can process the Image just after selecting it from gallery without displying in on the child widget
-        child: _image == null ? argonButton : Image.file(_image),
+        //child: _image == null ? argonButton : Image.file(_image),
+
+        //Using the ArgonButton to pick the image and replacing the button with image after statechange
+
+        child: _image == null ? ArgonButton(
+          height: 50,
+          width: 200,
+          borderRadius: 5.0,
+          color: Colors.lightGreen,
+          child: Text(
+            "Select From Gallery",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w700
+            ),
+          ),
+          loader: Container(
+            padding: EdgeInsets.all(10),
+            child: SpinKitRotatingCircle(
+              color: Colors.white,
+              // size: loaderWidth ,
+            ),
+          ),
+          onTap: (startLoading, stopLoading, btnState) {
+            if(btnState == ButtonState.Idle){
+              startLoading();
+              //Default Func
+              //await doNetworkRequest();
+              //Delaying for the loading circle animation
+              //await new Future.delayed(const Duration(seconds : 2));
+              getImage();
+              stopLoading();
+              //setImage(image);
+            }
+            //tempImage = await ImagePicker.pickImage(source: ImageSource.gallery);
+          },
+        ) : Image.file(_image) //Replacing Button with Image
       ),
 
       //A Floating Action Button Removed ! Alternative : Used An Argon Button
