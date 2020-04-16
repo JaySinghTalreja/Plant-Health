4ab9b773cbaf8a1428df814b1891a88842a46b15
JaySinghX <jaysinghtalreja@gmail.com>
2020-04-16 16:52:27 +0530

    Removed Old About Screen & About Module

--- lib/ui/about.dart
+++ lib/ui/about.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:about/about.dart';
+
 
 class About extends StatefulWidget {
   @override
@@ -9,61 +9,15 @@ class About extends StatefulWidget {
 class _AboutState extends State<About> {
   @override
   Widget build(BuildContext context) {
-    /*return new Scaffold(
-      body: new Center(
-        child: new Text("About Screen"),
+    return new Scaffold(
+      body: new Column(
+        children: <Widget>[
+          Container(
+            height: 350,
+            
+          ),
+        ],
       ),
-    );
-  }
-*/
-    final isIos = Theme.of(context).platform == TargetPlatform.iOS;
-    const Widget aboutPage = AboutPage(
-      //title: Text('About'),
-      applicationVersion: 'Version {{ version }}, build #{{ buildNumber }}',
-      applicationDescription: Text(
-        'About Developers',
-        textAlign: TextAlign.justify,
-      ),
-      applicationIcon: FlutterLogo(size: 100),
-      //applicationLegalese: '© David PHAM-VAN, {{ year }}',
-      children: <Widget>[
-        MarkdownPageListTile(
-          //filename: 'README.md',
-          title: Text('View Readme'),
-          icon: Icon(Icons.all_inclusive),
-        ),
-        /*MarkdownPageListTile(
-          filename: 'CHANGELOG.md',
-          title: Text('View Changelog'),
-          icon: Icon(Icons.view_list),
-        ),
-        MarkdownPageListTile(
-          filename: 'LICENSE.md',
-          title: Text('View License'),
-          icon: Icon(Icons.description),
-        ),
-        MarkdownPageListTile(
-          filename: 'CONTRIBUTING.md',
-          title: Text('Contributing'),
-          icon: Icon(Icons.share),
-        ),
-        MarkdownPageListTile(
-          filename: 'CODE_OF_CONDUCT.md',
-          title: Text('Code of conduct'),
-          icon: Icon(Icons.sentiment_satisfied),
-        ),
-        LicensesPageListTile(
-          title: Text('Open source Licenses'),
-          icon: Icon(Icons.favorite),
-        ),
-        */
-      ],
-    );
-    if (isIos) {
-    }
-    return const MaterialApp(
-      title: 'About App',
-      home: aboutPage,
     );
   }
 }
